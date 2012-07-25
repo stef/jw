13d5be8f4a4cc02907ad9c51f6860bc2367f91fa
stef <stefan.marsiske@gmail.com>
2012-07-25 17:40:18 +0200

    [mod] refactored

---
+++ jw.h
@@ -0,0 +1,15 @@
+#ifndef jw_h
+#define jw_h 1
+
+#define TILE_DEBUG 1
+//#define HAVE_JOYSTICK 1
+
+#ifdef HAVE_JOYSTICK
+#define buttonUp 7
+#define buttonDown 6
+#define buttonLeft 4
+#define buttonRight 5
+#define buttonFire 3
+#endif
+
+#endif
